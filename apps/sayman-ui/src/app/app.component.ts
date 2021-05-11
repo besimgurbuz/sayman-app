41943bd9f27a89339bedaa5d1921596cadc20d56
Besim Gurbuz <mbesimgurbuz@gmail.com>
2021-05-11 21:05:42 +0300

    initial commit

---
+++ apps/sayman-ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sayman-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'sayman-ui';
+}
