410c4a2a7d77578bfdc0124a154b9e0669e38f0a
Besim Gürbüz <mbesimgurbuz@gmail.com>
2021-08-31 03:37:47 +0300

    update @fullcalendar version and add an example

--- apps/sayman-ui/src/app/dashboard/dashboard.component.ts
+++ apps/sayman-ui/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/angular';
 
 @Component({
   selector: 'sayman-app-dashboard',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+  };
+
   constructor() {}
 
   ngOnInit(): void {}
--- apps/sayman-ui/src/app/dashboard/dashboard.module.ts
+++ apps/sayman-ui/src/app/dashboard/dashboard.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 
+FullCalendarModule.registerPlugins([dayGridPlugin, interactionPlugin]);
+
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [SharedModule, DashboardRoutingModule],
+  imports: [SharedModule, DashboardRoutingModule, FullCalendarModule],
 })
 export class DashboardModule {}
